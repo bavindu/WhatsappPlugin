c8df112adc25553cd0c41655c1ca3263920cdc4a
unknown <bavindubimsara.16@cse.mrt.ac.lk>
2020-03-07 20:45:30 +0530

    add schema export false

--- android/app/src/main/java/com/ideaboxapps/chatplus/database/MessageDatabase.java
+++ android/app/src/main/java/com/ideaboxapps/chatplus/database/MessageDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {WPMessage.class}, version = 1)
+@Database(entities = {WPMessage.class}, version = 1, exportSchema = false)
 public abstract class MessageDatabase extends RoomDatabase {
 
     private  static  MessageDatabase instance;
